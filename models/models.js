1d28134df0ce622bd9f4f2f497bd7761e3e592f4
Joel Tinx <wizard.joel@gmail.com>
2015-06-17 23:02:42 -0500

    revision errores 2

--- models/models.js
+++ models/models.js
@@ -1,7 +1,5 @@
 var path = require('path');
 
-// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
-// SQLite   DATABASE_URL = sqlite://:@:/
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var DB_name  = (url[6]||null);
 var user     = (url[2]||null);
@@ -12,21 +10,18 @@ var port     = (url[5]||null);
 var host     = (url[4]||null);
 var storage  = process.env.DATABASE_STORAGE;
 
-// Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
-// Usar BBDD SQLite o Postgres
 var sequelize = new Sequelize(DB_name, user, pwd,
   { dialect:  protocol,
     protocol: protocol,
     port:     port,
     host:     host,
-    storage:  storage,  // solo SQLite (.env)
-    omitNull: true      // solo Postgres
+    storage:  storage,
+    omitNull: true
   }
 );
 
-// Importar definicion de la tabla Quiz
 var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(quiz_path);
 exports.Quiz = Quiz;
