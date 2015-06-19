7e850197252de4e338b431337656f3c5efa732ac
Joel Tinx <wizard.joel@gmail.com>
2015-06-19 18:38:38 -0500

    Commit > Editar preguntas

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,8 @@ router.get('/quizes/nuevo', quizController.nuevo)
 router.get('/quizes/:quizId(\\d+)', quizController.show)
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer)
 router.post('/quizes/guardar', quizController.guardar)
+router.get('/quizes/:quizId(\\d+)/editar', quizController.editar)
+router.put('/quizes/:quizId(\\d+)', quizController.update)
 
 router.get('/creditos', function(req, res){
   res.render('creditos')
